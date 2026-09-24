cebbc5ce9fa1c82cc90cb57f388acfc5fed3dcd3
Avel-Dev <arumugaveldev@gmail.com>
2026-09-24 19:50:15 +0530

    feat: implement lexer tokenization
    - Add keyword and literal recognition
    - Add arithmetic, relational, logical, assignment and bitwise operators
    - Add comment handling and punctuation tokens
    - Add multi-character operator lookahead

--- src/lexer.c
+++ src/lexer.c
@@ -13,18 +13,64 @@ typedef enum {
 	TOKEN_FLOAT_LITERAL,
 	TOKEN_STRING_LITERAL,
 
+	// Keywords
+	TOKEN_INT,
+	TOKEN_FLOAT,
+	TOKEN_CHAR,
+	TOKEN_VOID,
+	TOKEN_IF,
+	TOKEN_ELSE,
+	TOKEN_WHILE,
+	TOKEN_FOR,
+	TOKEN_RETURN,
+
+	// Arithmetic operators
 	TOKEN_PLUS,
 	TOKEN_MINUS,
 	TOKEN_STAR,
 	TOKEN_SLASH,
 	TOKEN_MOD,
+	TOKEN_INCREMENT,
+	TOKEN_DECREMENT,
+
+	// relational operators
+	TOKEN_EQUALS,
+	TOKEN_NOT_EQUALS,
+	TOKEN_LESS_THAN,
+	TOKEN_MORE_THAN,
+	TOKEN_LESS_EQUALS,
+	TOKEN_MORE_EQUALS,
+
+	// Assignment
+	TOKEN_ASSIGN,
+	TOKEN_ADD_ASSIGN,
+	TOKEN_SUB_ASSIGN,
+	TOKEN_SLASH_ASSIGN,
+	TOKEN_MOD_ASSIGN,
+	TOKEN_MUL_ASSIGN,
+
+	// Logical
+	TOKEN_AND,
+	TOKEN_OR,
+	TOKEN_NOT,
+
+	// BITWISE
+	TOKEN_BIT_AND,
+	TOKEN_BIT_OR,
+	TOKEN_BIT_NOT,
+	TOKEN_BIT_XOR,
+	TOKEN_BIT_LSHIFT,
+	TOKEN_BIT_RSHIFT,
 
 	TOKEN_LPAREN,
 	TOKEN_RPAREN,
 	TOKEN_LBRACE,
 	TOKEN_RBRACE,
 
-	TOKEN_SEMICOLON
+	TOKEN_SEMICOLON,
+	TOKEN_ERROR,
+
+	TOKEN_COMMENT
 } TokenType;
 
 typedef struct {
@@ -43,31 +89,195 @@ void lexer_init(Lexer* lexer, const char* source) {
 	lexer->position = 0;
 }
 
+TokenType keyword_type(const char* start, size_t length) {
+	if (length == 3 && strncmp(start, "int", 3) == 0) {
+		return TOKEN_INT;
+	} else if (length == 4 && strncmp(start, "float", 4) == 0) {
+		return TOKEN_FLOAT;
+	} else if (length == 4 && strncmp(start, "char", 4) == 0) {
+		return TOKEN_CHAR;
+	} else if (length == 4 && strncmp(start, "void", 4) == 0) {
+		return TOKEN_VOID;
+	} else if (length == 2 && strncmp(start, "if", 2) == 0) {
+		return TOKEN_IF;
+	} else if (length == 4 && strncmp(start, "else", 4) == 0) {
+		return TOKEN_ELSE;
+	} else if (length == 5 && strncmp(start, "while", 5) == 0) {
+		return TOKEN_WHILE;
+	} else if (length == 3 && strncmp(start, "for", 3) == 0) {
+		return TOKEN_FOR;
+	} else if (length == 6 && strncmp(start, "return", 6) == 0) {
+		return TOKEN_RETURN;
+	}
+
+	return TOKEN_IDENTIFIER;
+}
+
 Token* lexer_next(Lexer* lexer) {
+	if (lexer->source[lexer->position] == '\0') return NULL;
 	Token* token = malloc(sizeof(Token));
 	const char* source = lexer->source;
 
-	int curr_max = 10;
-	int c;
+	unsigned char c;
 	while ((c = source[lexer->position]) != '\0') {
-		if (c == '+' || c == '-' || c == '*' || c == '/' || c == '%') {
-			if (c == '+') {
+		if (isspace(c)) {
+			lexer->position++;
+			continue;
+		}
+		if (isalpha(c) || c == '_') {
+			size_t start = lexer->position;
+			token->start = &source[start];
+			while ((c = source[lexer->position]) != '\0' &&
+			       (isalnum(c) || c == '_')) {
+				lexer->position++;
+			}
+			token->length = lexer->position - start;
+			token->type = keyword_type(token->start, token->length);
+			return token;
+
+		} else if (isdigit(c)) {
+			token->type = TOKEN_INT_LITERAL;
+			size_t start = lexer->position;
+			token->start = &source[start];
+			while (isdigit(source[lexer->position])) {
+				lexer->position++;
+			}
+			if (source[lexer->position] == '.') {
+				token->type = TOKEN_FLOAT_LITERAL;
+				lexer->position++;
+				while (isdigit(source[lexer->position])) {
+					lexer->position++;
+				}
+			}
+			token->length = lexer->position - start;
+			printf("DEBUG position=%zu char='%c'\n", lexer->position,
+			       source[lexer->position]);
+			return token;
+		} else if (c == '"') {
+			token->type = TOKEN_STRING_LITERAL;
+			size_t start = lexer->position++;
+			token->start = &source[start];
+			while (source[lexer->position] != '\0' &&
+			       source[lexer->position] != '"') {
+				lexer->position++;
+			}
+			if (source[lexer->position] == '\0') {
+				token->type = TOKEN_ERROR;
+				token->length = lexer->position - start;
+				return token;
+			}
+			lexer->position++;
+			token->length = lexer->position - start;
+			return token;
+		} else {
+			token->type = TOKEN_ERROR;
+			char sec = source[lexer->position + 1];
+			if (c == '=') {
+				token->type = TOKEN_ASSIGN;
+				if (sec == '=') {
+					token->type = TOKEN_EQUALS;
+					token->length = 2;
+					token->start = &source[lexer->position];
+					lexer->position += 2;
+					return token;
+				}
+			} else if (c == '+') {
 				token->type = TOKEN_PLUS;
+				if (sec == '+') {
+					token->type = TOKEN_INCREMENT;
+					token->length = 2;
+					token->start = &source[lexer->position];
+					lexer->position += 2;
+					return token;
+				} else if (sec == '=') {
+					token->type = TOKEN_ADD_ASSIGN;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+
 			} else if (c == '-') {
 				token->type = TOKEN_MINUS;
+				char sec = source[lexer->position + 1];
+				if (sec == '-') {
+					token->type = TOKEN_DECREMENT;
+					token->length = 2;
+					token->start = &source[lexer->position];
+					lexer->position += 2;
+					return token;
+				} else if (sec == '=') {
+					token->type = TOKEN_SUB_ASSIGN;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+
 			} else if (c == '*') {
 				token->type = TOKEN_STAR;
+				if (sec == '=') {
+					token->type = TOKEN_MUL_ASSIGN;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
 			} else if (c == '/') {
 				token->type = TOKEN_SLASH;
+				if (sec == '=') {
+					token->type = TOKEN_SLASH_ASSIGN;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				} else if (sec == '/') {
+					token->type = TOKEN_COMMENT;
+					token->start = &source[lexer->position];
+					size_t start = lexer->position;
+					while (c != '\n' && c != '\0') {
+						c = source[lexer->position++];
+					}
+					if (c == '\n') lexer->position++;
+
+					token->length = lexer->position - start;
+					return token;
+				} else if (sec == '*') {
+					token->type = TOKEN_COMMENT;
+					token->start = &source[lexer->position];
+					size_t start = lexer->position;
+					lexer->position += 2;
+					c = source[lexer->position];
+					sec = source[lexer->position + 1];
+
+					while (!(c == '*' && sec == '/')) {
+						if (sec == '\0') break;
+						lexer->position++;
+						c = source[lexer->position];
+						sec = source[lexer->position + 1];
+					}
+					if (sec == '/') {
+						lexer->position += 2;
+					} else
+						lexer->position++;
+					token->length = lexer->position - start;
+					return token;
+				}
 			} else if (c == '%') {
 				token->type = TOKEN_MOD;
-			}
-			token->length = 1;
-			token->start = &source[lexer->position++];
-			return token;
-			// emit operator
-		} else if (c == '{' || c == '}' || c == '(' || c == ')') {
-			if (c == '{') {
+				if (sec == '=') {
+					token->type = TOKEN_MOD_ASSIGN;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+			} else if (c == '{') {
 				token->type = TOKEN_LBRACE;
 			} else if (c == '}') {
 				token->type = TOKEN_RBRACE;
@@ -75,42 +285,86 @@ Token* lexer_next(Lexer* lexer) {
 				token->type = TOKEN_LPAREN;
 			} else if (c == ')') {
 				token->type = TOKEN_RPAREN;
+			} else if (c == ';') {
+				token->type = TOKEN_SEMICOLON;
+			} else if (c == '<') {
+				token->type = TOKEN_LESS_THAN;
+				if (sec == '=') {
+					token->type = TOKEN_LESS_EQUALS;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				} else if (sec == '<') {
+					token->type = TOKEN_BIT_LSHIFT;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+			} else if (c == '>') {
+				token->type = TOKEN_MORE_THAN;
+				if (sec == '=') {
+					token->type = TOKEN_MORE_EQUALS;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				} else if (sec == '>') {
+					token->type = TOKEN_BIT_RSHIFT;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+			} else if (c == '&') {
+				token->type = TOKEN_BIT_AND;
+				if (sec == '&') {
+					token->type = TOKEN_AND;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+			} else if (c == '|') {
+				token->type = TOKEN_BIT_OR;
+				if (sec == '|') {
+					token->type = TOKEN_OR;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+			} else if (c == '!') {
+				token->type = TOKEN_NOT;
+				if (sec == '=') {
+					token->type = TOKEN_NOT_EQUALS;
+					token->length = 2;
+					token->start = &source[lexer->position];
+
+					lexer->position += 2;
+					return token;
+				}
+
+			} else if (c == '~') {
+				token->type = TOKEN_BIT_NOT;
+			} else if (c == '^') {
+				token->type = TOKEN_BIT_XOR;
 			}
+
 			token->length = 1;
 			token->start = &source[lexer->position++];
 			return token;
-		} else if (isalpha(c) || c == '_') {
-			token->type = TOKEN_IDENTIFIER;
-			size_t start = lexer->position;
-			token->start = &lexer->source[start];
-			while ((c = source[lexer->position]) != '\0' &&
-			       (isalnum(c) || c == '_')) {
-				lexer->position++;
-			}
-			token->length = lexer->position - start;
-			return token;
-
-		} else if (isdigit(c)) {
-			token->type = TOKEN_INT_LITERAL;
-			size_t start = lexer->position;
-			token->start = &lexer->source[start];
-			while (isdigit(lexer->source[lexer->position])) {
-				lexer->position++;
-			}
-			if (lexer->source[lexer->position] == '.') {
-				token->type = TOKEN_FLOAT_LITERAL;
-				while (isdigit(lexer->source[lexer->position])) {
-					lexer->position++;
-				}
-			}
-			token->length = lexer->position - start;
-			return token;
 		}
-		lexer->position++;
 	}
-
 	token->type = TOKEN_EOF;
 	token->length = 0;
-	token->start = &lexer->source[lexer->position];
+	token->start = &source[lexer->position];
 	return token;
 }
--- src/main.c
+++ src/main.c
@@ -7,37 +7,107 @@
 const char* token_type_name(TokenType type) {
 	switch (type) {
 	case TOKEN_EOF:
-		return "EOF";
+		return "TOKEN_EOF";
 	case TOKEN_IDENTIFIER:
-		return "IDENTIFIER";
+		return "TOKEN_IDENTIFIER";
+
 	case TOKEN_INT_LITERAL:
-		return "INT_LITERAL";
+		return "TOKEN_INT_LITERAL";
 	case TOKEN_FLOAT_LITERAL:
-		return "FLOAT_LITERAL";
+		return "TOKEN_FLOAT_LITERAL";
 	case TOKEN_STRING_LITERAL:
-		return "STRING_LITERAL";
+		return "TOKEN_STRING_LITERAL";
+
+	// Keywords
+	case TOKEN_INT:
+		return "TOKEN_INT";
+	case TOKEN_FLOAT:
+		return "TOKEN_FLOAT";
+	case TOKEN_CHAR:
+		return "TOKEN_CHAR";
+	case TOKEN_VOID:
+		return "TOKEN_VOID";
+	case TOKEN_IF:
+		return "TOKEN_IF";
+	case TOKEN_ELSE:
+		return "TOKEN_ELSE";
+	case TOKEN_WHILE:
+		return "TOKEN_WHILE";
+	case TOKEN_FOR:
+		return "TOKEN_FOR";
+	case TOKEN_RETURN:
+		return "TOKEN_RETURN";
+
+	// Arithmetic operators
 	case TOKEN_PLUS:
-		return "PLUS";
+		return "TOKEN_PLUS";
 	case TOKEN_MINUS:
-		return "MINUS";
+		return "TOKEN_MINUS";
 	case TOKEN_STAR:
-		return "STAR";
+		return "TOKEN_STAR";
 	case TOKEN_SLASH:
-		return "SLASH";
+		return "TOKEN_SLASH";
 	case TOKEN_MOD:
-		return "MOD";
+		return "TOKEN_MOD";
+	case TOKEN_INCREMENT:
+		return "TOKEN_INCREMENT";
+	case TOKEN_DECREMENT:
+		return "TOKEN_DECREMENT";
+
+	// Relational operators
+	case TOKEN_EQUALS:
+		return "TOKEN_EQUALS";
+	case TOKEN_NOT_EQUALS:
+		return "TOKEN_NOT_EQUALS";
+	case TOKEN_LESS_THAN:
+		return "TOKEN_LESS_THAN";
+	case TOKEN_MORE_THAN:
+		return "TOKEN_MORE_THAN";
+	case TOKEN_LESS_EQUALS:
+		return "TOKEN_LESS_EQUALS";
+	case TOKEN_MORE_EQUALS:
+		return "TOKEN_MORE_EQUALS";
+
+	// Assignment
+	case TOKEN_ASSIGN:
+		return "TOKEN_ASSIGN";
+	case TOKEN_ADD_ASSIGN:
+		return "TOKEN_ADD_ASSIGN";
+	case TOKEN_SUB_ASSIGN:
+		return "TOKEN_SUB_ASSIGN";
+	case TOKEN_SLASH_ASSIGN:
+		return "TOKEN_SLASH_ASSIGN";
+	case TOKEN_MOD_ASSIGN:
+		return "TOKEN_MOD_ASSIGN";
+	case TOKEN_MUL_ASSIGN:
+		return "TOKEN_MUL_ASSIGN";
+
+	// Logical
+	case TOKEN_AND:
+		return "TOKEN_AND";
+	case TOKEN_OR:
+		return "TOKEN_OR";
+	case TOKEN_NOT:
+		return "TOKEN_NOT";
+
 	case TOKEN_LPAREN:
-		return "LPAREN";
+		return "TOKEN_LPAREN";
 	case TOKEN_RPAREN:
-		return "RPAREN";
+		return "TOKEN_RPAREN";
 	case TOKEN_LBRACE:
-		return "LBRACE";
+		return "TOKEN_LBRACE";
 	case TOKEN_RBRACE:
-		return "RBRACE";
+		return "TOKEN_RBRACE";
+
 	case TOKEN_SEMICOLON:
-		return "SEMICOLON";
+		return "TOKEN_SEMICOLON";
+	case TOKEN_ERROR:
+		return "TOKEN_ERROR";
+	case TOKEN_COMMENT:
+		return "TOKEN_COMMENT";
+
 	default:
-		return "UNKNOWN";
+		return "TOKEN_UNKNOWN";
 	}
 }
 
@@ -50,10 +120,14 @@ int main() {
 		perror("test.unn");
 		return 1;
 	}
-
-	char source[100];
-	while (i < sizeof(source) - 1 && (c = fgetc(file)) != EOF) {
+	int count = 100;
+	char* source = malloc(count * sizeof(char));
+	while (source && (c = fgetc(file)) != EOF) {
 		source[i++] = (char)c;
+		if (i == count - 1) {
+			count *= 2;
+			source = realloc(source, count * sizeof(char));
+		}
 	}
 	source[i++] = '\0';
 	fclose(file);
@@ -61,8 +135,10 @@ int main() {
 	lexer_init(lexer, source);
 
 	Token* token;
-	while ((token = lexer_next(lexer)) != NULL && token->type != TOKEN_EOF) {
-		printf("Token: %s\n", token_type_name(token->type));
+	while ((token = lexer_next(lexer)) != NULL) {
+		printf("Token: %s Content: %.*s\n", token_type_name(token->type),
+		       (int)token->length, token->start);
+		free(token);
 	}
 
 	free(lexer);
